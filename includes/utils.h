68c579375e45965d4e9750295caf88d7a19ba5b4
nfukada <nfukada@student.42tokyo.jp>
2021-02-13 23:36:14 +0900

    feat: 構造体t_env追加

--- includes/utils.h
+++ includes/utils.h
@@ -6,13 +6,20 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 20:13:16 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/12 20:13:19 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/13 23:35:38 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
-void	error_exit(void);
+typedef struct	s_env
+{
+	char			*name;
+	char			*value;
+	struct s_env	*next;
+}				t_env;
+
+void			error_exit(void);
 
 #endif
