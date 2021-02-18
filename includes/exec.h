c1357d637e54244d77180a78c1bd54deadd89523
nfukada <nfukada@student.42tokyo.jp>
2021-02-18 15:40:31 +0900

    feat(exec): t_command構造体追加
    t_commandは単方向連結リスト。argsは一旦t_tokenリストで持つように

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 20:07:01 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/13 23:37:03 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/18 15:28:01 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 # define EXEC_H
 
 # include "utils.h"
+# include "lexer.h"
 
-void	exec_command(char **args, t_env *envs);
+typedef struct	s_command
+{
+	t_token				*args;
+	struct s_command	*next;
+}				t_command;
+
+void			exec_command(char **args, t_env *envs);
 
 #endif
