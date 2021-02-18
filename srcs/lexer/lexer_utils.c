338a023a847967edd63e1070e89fe43190437e65
Taisei Toya <tytaisei0102@gmail.com>
2021-02-18 20:06:20 +0100

    refactor: 不要変数を削除

--- srcs/lexer/lexer_utils.c
+++ srcs/lexer/lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 20:01:50 by totaisei          #+#    #+#             */
-/*   Updated: 2021/02/18 20:01:52 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/02/18 20:05:00 by totaisei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,17 +56,14 @@ void	general_state(t_tokeniser *toker, t_token_type type, char *str)
 		if (type == CHAR_QOUTE)
 		{
 			toker->state = STATE_IN_QUOTE;
-			toker->quote_start = &toker->token->data[toker->tok_i - 1];
 		}
 		else if (type == CHAR_DQUOTE)
 		{
 			toker->state = STATE_IN_DQUOTE;
-			toker->quote_start = &toker->token->data[toker->tok_i - 1];
 		}
 		else
 		{
 			toker->state = STATE_GENERAL;
-			toker->quote_start = NULL;
 		}
 		toker->token->type = TOKEN;
 	}
