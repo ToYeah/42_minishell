b4bb370c4aa76fa5658f24383d162e69e8b349cd
nfukada <nfukada@student.42tokyo.jp>
2021-02-19 22:12:25 +0900

    feat(exec): t_redirect, t_redirect_typeを追加

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 20:07:01 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/18 15:28:01 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/19 22:16:21 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,24 @@
 # include "utils.h"
 # include "lexer.h"
 
+typedef enum	e_redirect_type
+{
+	REDIR_INPUT,
+	REDIR_OUTPUT,
+	REDIR_APPEND_OUTPUT
+}				t_redirect_type;
+
+typedef struct	s_redirect
+{
+	t_redirect_type		type;
+	t_token				*filename;
+	struct s_redirect	*next;
+}				t_redirect;
+
 typedef struct	s_command
 {
 	t_token				*args;
+	t_redirect			*redirects;
 }				t_command;
 
 void			exec_command(char **args, t_env *envs);
