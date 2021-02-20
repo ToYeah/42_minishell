a7384eb694d8a3a96048ba99dab2ba2432de4744
nfukada <nfukada@student.42tokyo.jp>
2021-02-21 00:12:04 +0900

    feat(node): node情報を標準出力ではなくファイル(ast.dot)に出力するように

--- srcs/parser/node_print.c
+++ srcs/parser/node_print.c
@@ -6,67 +6,93 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 15:36:06 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/18 15:51:40 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/21 00:08:56 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <fcntl.h>
 #include "lexer.h"
 #include "parser.h"
 
-#define HEADER "    "
+#define DOT_FILE_NAME	"ast.dot"
+#define DOT_INDENT		"    "
 
-void	print_command_args(t_token *args)
+void	print_command_args(t_token *args, int fd)
 {
-	printf("{");
 	while (args)
 	{
-		printf("%s", args->data);
+		ft_putstr_fd(args->data, fd);
 		args = args->next;
 		if (args)
-			printf(", ");
+			ft_putstr_fd(", ", fd);
 	}
-	printf("}");
 }
 
-void	print_node_type(t_node *node)
+void	print_node_type(t_node *node, int fd)
 {
 	if (node->type == NODE_SEMICOLON)
-		printf("SEMICOLON");
+		ft_putstr_fd("SEMICOLON", fd);
 	else if (node->type == NODE_PIPE)
-		printf("PIPE");
+		ft_putstr_fd("PIPE", fd);
 	else if (node->type == NODE_COMMAND)
-		printf("COMMAND");
+		ft_putstr_fd("COMMAND", fd);
 	else
-		printf("unknown");
+		ft_putstr_fd("unknown", fd);
 }
 
-void	print_node(t_node *node)
+/*
+** print node and edge definitions
+**     NODE_{node} [label="{node->type}\n{args}"];
+**     NODE_{node} -> NODE_{node->left;}
+**     NODE_{node} -> NODE_{node->right;}
+*/
+
+void	print_node(t_node *node, int fd)
 {
-	printf(HEADER"NODE_%p [label=\"", node);
-	print_node_type(node);
-	printf("\\n");
+	ft_putstr_fd(DOT_INDENT"NODE_", fd);
+	ft_puthex_ul_fd((unsigned long)node, 0, fd);
+	ft_putstr_fd(" [label=\"", fd);
+	print_node_type(node, fd);
+	ft_putstr_fd("\\n", fd);
 	if (node->type == NODE_COMMAND)
-		print_command_args(node->command->args);
-	printf("\"];\n");
+		print_command_args(node->command->args, fd);
+	ft_putstr_fd("\"];\n", fd);
 	if (node->left)
-		printf(HEADER"NODE_%p -> NODE_%p;\n", node, node->left);
+	{
+		ft_putstr_fd(DOT_INDENT"NODE_", fd);
+		ft_puthex_ul_fd((unsigned long)node, 0, fd);
+		ft_putstr_fd(" -> NODE_", fd);
+		ft_puthex_ul_fd((unsigned long)node->left, 0, fd);
+		ft_putendl_fd(";", fd);
+	}
 	if (node->right)
-		printf(HEADER"NODE_%p -> NODE_%p;\n", node, node->right);
+	{
+		ft_putstr_fd(DOT_INDENT"NODE_", fd);
+		ft_puthex_ul_fd((unsigned long)node, 0, fd);
+		ft_putstr_fd(" -> NODE_", fd);
+		ft_puthex_ul_fd((unsigned long)node->right, 0, fd);
+		ft_putendl_fd(";", fd);
+	}
 }
 
-void	print_nodes_rec(t_node *node)
+void	print_nodes_rec(t_node *node, int fd)
 {
 	if (!node)
 		return ;
-	print_node(node);
-	print_nodes_rec(node->left);
-	print_nodes_rec(node->right);
+	print_node(node, fd);
+	print_nodes_rec(node->left, fd);
+	print_nodes_rec(node->right, fd);
 }
 
 void	print_nodes(t_node *node)
 {
-	printf("digraph AST {\n");
-	print_nodes_rec(node);
-	printf("}\n");
+	int	fd;
+
+	fd = open(DOT_FILE_NAME, O_TRUNC | O_CREAT | O_WRONLY, 0755);
+	if (fd < 0)
+		error_exit();
+	ft_putstr_fd("digraph AST {\n", fd);
+	print_nodes_rec(node, fd);
+	ft_putstr_fd("}\n", fd);
+	close(fd);
 }
