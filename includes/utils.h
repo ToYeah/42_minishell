b505fc37fedf10fd70018670c7115b249cad49e9
nfukada <nfukada@student.42tokyo.jp>
2021-02-21 00:55:22 +0900

    chore(utils): utils.hにprint_unexpected_token_error()追加
    - utils.hとtoken.hが相互includeしているのでt_tokenの宣言を入れてます

--- includes/utils.h
+++ includes/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 20:13:16 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/19 11:17:05 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/02/21 00:33:08 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,29 @@
 # define UTILS_H
 
 # include <stddef.h>
+# include "token.h"
 
-typedef struct	s_env
+typedef struct			s_env
 {
 	char			*name;
 	char			*value;
 	struct s_env	*next;
-}				t_env;
+}						t_env;
 
-void			error_exit(void);
+typedef struct s_token	t_token;
 
-t_env			*create_envs_from_environ(void);
-void			print_envs(t_env *envs);
-char			**generate_environ(t_env *envs);
-void			add_env(t_env **envs, t_env *new_env);
-void			del_env(t_env **envs, char *name);
+void					error_exit(void);
+void					print_unexpected_token_error(t_token *token);
 
-t_env			*get_last_env(t_env *envs);
-size_t			get_env_size(t_env *envs);
-t_env			*create_new_env(char *env_str);
-const char		*search_env(t_env *envs, char *name);
+t_env					*create_envs_from_environ(void);
+void					print_envs(t_env *envs);
+char					**generate_environ(t_env *envs);
+void					add_env(t_env **envs, t_env *new_env);
+void					del_env(t_env **envs, char *name);
+
+t_env					*get_last_env(t_env *envs);
+size_t					get_env_size(t_env *envs);
+t_env					*create_new_env(char *env_str);
+const char				*search_env(t_env *envs, char *name);
 
 #endif
