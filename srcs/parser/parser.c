110b33ab6d9824adb2dceb74d3e8c1eacebc59f8
nfukada <nfukada@student.42tokyo.jp>
2021-02-22 00:31:39 +0900

    fix(parser): redirect=-2を定数に修正

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 01:11:20 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/21 22:56:52 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/22 00:31:05 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static t_bool	parse_io_redirect(t_token **tokens, t_node *command_node)
 	if ((*tokens)->type == IO_NUMBER)
 	{
 		if (ft_atoi_limit((*tokens)->data, &redirect->fd) == FALSE)
-			redirect->fd = -2;
+			redirect->fd = REDIR_FD_OUT_OF_RANGE;
 		*tokens = (*tokens)->next;
 	}
 	if (set_redirect_type(*tokens, redirect) == FALSE)
