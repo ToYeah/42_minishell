bb21afc6e5ce8c5f3b766a2ef486cb0a1f8c8da3
nfukada <nfukada@student.42tokyo.jp>
2021-02-24 00:18:27 +0900

    refactor(main): 読み取った行からコマンド実行する部分を関数に切り出し
    -c対応で共通の処理になるため
    未使用のfree_array()は削除

--- srcs/main.c
+++ srcs/main.c
@@ -19,18 +19,20 @@
 
 t_env	*g_envs;
 
-void	free_array(char **array)
+void	run_commandline(char *line)
 {
-	size_t	i;
+	t_token	*tokens;
+	t_token	*start_token;
+	t_node	*nodes;
 
-	i = 0;
-	while (array[i])
-	{
-		free(array[i]);
-		array[i] = NULL;
-	}
-	free(array);
-	array = NULL;
+	tokens = tokenise(line, FALSE);
+	start_token = tokens;
+	if (parse_complete_command(&nodes, &tokens) == FALSE)
+		print_unexpected_token_error(tokens);
+	else
+		exec_nodes(nodes);
+	del_token_list(&start_token);
+	del_node_list(nodes);
 }
 
 void	loop_shell(void)
@@ -38,9 +40,6 @@ void	loop_shell(void)
 	int		status;
 	char	*line;
 	int		gnl_result;
-	t_token	*tokens;
-	t_token	*start_token;
-	t_node	*nodes;
 
 	status = 1;
 	gnl_result = 1;
@@ -50,15 +49,8 @@ void	loop_shell(void)
 			ft_putstr_fd(SHELL_PROMPT, STDERR_FILENO);
 		if ((gnl_result = ft_get_next_line(STDIN_FILENO, &line)) < 0)
 			error_exit();
-		tokens = tokenise(line, 0);
-		start_token = tokens;
-		if (parse_complete_command(&nodes, &tokens) == FALSE)
-			print_unexpected_token_error(tokens);
-		else
-			exec_nodes(nodes);
+		run_commandline(line);
 		free(line);
-		del_token_list(&start_token);
-		del_node_list(nodes);
 	}
 }
 
