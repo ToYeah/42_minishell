295f7519ea4766bb1e66ca8078e24a1b63d00d7a
nfukada <nfukada@student.42tokyo.jp>
2021-02-27 12:38:54 +0900

    fix: syntax error時にg_statusを258にセットするように

--- includes/const.h
+++ includes/const.h
@@ -6,14 +6,16 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 19:46:48 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/24 14:54:13 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/27 12:34:40 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONST_H
 # define CONST_H
 
-# define SHELL_PROMPT	"\e[32mminishell > \e[0m"
+# define SHELL_PROMPT			"\e[32mminishell > \e[0m"
+
+# define STATUS_SYNTAX_ERROR	258
 
 # ifndef DEBUG
 #  define DEBUG			FALSE
