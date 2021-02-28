10febd62348ae9604ee7a76f4583160c0242129f
Taisei Toya <tytaisei0102@gmail.com>
2021-02-28 11:10:48 +0100

    feat(utils.h,env_utils2.c): get_envを追加

--- includes/utils.h
+++ includes/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 20:13:16 by nfukada           #+#    #+#             */
-/*   Updated: 2021/02/27 23:26:49 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/02/28 11:10:16 by totaisei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ t_env			*get_last_env(t_env *envs);
 size_t			get_env_size(t_env *envs);
 t_env			*create_new_env(char *env_str);
 const char		*search_env(char *name);
+t_env			*get_env(const char *name);
 
 #endif
