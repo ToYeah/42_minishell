92a46710dfe52a7d6bce7777e857dde9da144b85
nfukada <nfukada@student.42tokyo.jp>
2021-03-03 19:56:06 +0900

    chore(const.h): STATUS_CMD_NOT_EXECUTABLEを追加

--- includes/const.h
+++ includes/const.h
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 19:46:48 by nfukada           #+#    #+#             */
-/*   Updated: 2021/03/03 14:40:03 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/03/03 17:51:59 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 # define SHELL_PROMPT			"\e[32mminishell > \e[0m"
 
-# define STATUS_SYNTAX_ERROR	258
-# define STATUS_TOKEN_ERROR		2
-# define STATUS_CMD_NOT_FOUND	127
+# define STATUS_SYNTAX_ERROR		258
+# define STATUS_TOKEN_ERROR			2
+# define STATUS_CMD_NOT_FOUND		127
+# define STATUS_CMD_NOT_EXECUTABLE	126
 
 # ifndef DEBUG
 #  define DEBUG			FALSE
