9fe4352c97ea3703f780a78c96ad1d201bf79b37
Taisei Toya <tytaisei0102@gmail.com>
2021-03-04 14:39:53 +0100

    refactor(path_utils): path関連のリファクタリング
    build_full_pathで[./aaa//a]のような'/'がつづくpathを作らないよう修正
    join_pathの内部でprevの最後の文字が'/'だった時は追加した文字を消すよう修正

--- srcs/utils/path_utils.c
+++ srcs/utils/path_utils.c
@@ -6,27 +6,56 @@
 /*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 15:32:52 by totaisei          #+#    #+#             */
-/*   Updated: 2021/03/04 11:59:11 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/03/04 14:38:47 by totaisei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 #include "libft.h"
 
+void	correct_path(char **str)
+{
+	char	**split;
+	char	*res;
+	size_t	index;
+
+	if (!str || !*str)
+		return ;
+	index = 0;
+	if (!(split = ft_split(*str, '/')) ||
+		!(res = malloc(sizeof(char) * (ft_strlen(*str) + 1))))
+		error_exit(NULL);
+	ft_safe_free_char(str);
+	*str = res;
+	*res++ = '/';
+	while (split[index])
+	{
+		if (index != 0)
+			*res++ = '/';
+		res = ft_strcpy_forward(res, split[index]);
+		index++;
+	}
+	*res = '\0';
+	ft_safe_free_split(&split);
+}
+
 char	*join_path(const char *prev, const char *next)
 {
-	char *tmp;
-	char *res;
+	char	*tmp;
+	char	*res;
+	size_t	index;
 
 	if (!prev || !next)
 		return (NULL);
 	tmp = NULL;
 	res = NULL;
-	if (!(tmp = ft_strjoin(prev, "/")) ||
-		!(res = ft_strjoin(tmp, next)))
-	{
+	if (!(tmp = ft_strjoin(prev, "/")))
 		error_exit(NULL);
-	}
+	index = ft_strlen(tmp);
+	if (index >= 2 && tmp[index - 2] == '/')
+		tmp[index - 1] = '\0';
+	if (!(res = ft_strjoin(tmp, next)))
+		error_exit(NULL);
 	free(tmp);
 	return (res);
 }
@@ -54,10 +83,11 @@ char	*build_full_path(char *path, const char *cmd)
 	}
 	free(cwd);
 	free(tmp);
+	correct_path(&res);
 	return (res);
 }
 
-t_bool		is_directory(const char *path)
+t_bool	is_directory(const char *path)
 {
 	t_stat path_stat;
 
