aa2b22e39f8411126d2b13d04928957643f48b87
Taisei Toya <tytaisei0102@gmail.com>
2021-03-04 16:57:52 +0100

    feat(mini_cd): 引数が一つの時の処理を追加

--- srcs/builtin/mini_cd.c
+++ srcs/builtin/mini_cd.c
@@ -6,7 +6,7 @@
 /*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 10:50:50 by totaisei          #+#    #+#             */
-/*   Updated: 2021/03/04 16:15:16 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/03/04 16:56:49 by totaisei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ char	*get_new_pwd(char *path, t_bool is_canon_path,t_bool is_abs_path)
 	return (new_pwd);
 }
 
-int		change_dir_process(char *cd_path, char *arg, t_bool is_canon_path)
+int		change_dir_process(char *cd_path,const char *arg, t_bool is_canon_path)
 {
 	int		res;
 	int		err;
@@ -92,7 +92,7 @@ int		change_dir_process(char *cd_path, char *arg, t_bool is_canon_path)
 	return (res);
 }
 
-t_bool	set_cd_path(char **cd_path, char *arg)
+t_bool	set_cd_path(char **cd_path, const char *arg)
 {
 	char		*canon_path;
 	char		*physical_path;
@@ -119,13 +119,46 @@ t_bool	set_cd_path(char **cd_path, char *arg)
 	return res;
 }
 
+const char	*set_cd_destination(char **args)
+{
+	t_env	*home_env;
+	size_t	index;
+
+	index = 0;
+	while (args[index])
+		index++;
+	if (index >= 3)
+	{
+		print_error("too many arguments", "cd");
+		return (NULL);
+	}
+	if (index == 1)
+	{
+		if (!(home_env = get_env("HOME")))
+		{
+			print_error("HOME not set", "cd");
+			return (NULL);
+		}
+		if (!home_env->value)
+			return ("");
+		return (home_env->value);
+	}
+	return (args[1]);
+}
+
+
 int		exec_cd(char **args)
 {
 	char *cd_path;
 	t_bool is_canon_path;
+	const char	*destination;
 
 	extern char *g_pwd;
-	is_canon_path = set_cd_path(&cd_path, args[1]);
-	change_dir_process(cd_path, args[1], is_canon_path);
-	return (0);
+
+	if (!(destination = set_cd_destination(args)))
+		return (EXIT_FAILURE);
+	is_canon_path = set_cd_path(&cd_path, destination);
+	change_dir_process(cd_path, destination, is_canon_path);
+	ft_safe_free_char(&cd_path);
+	return (EXIT_SUCCESS);
 }
