c6c3ede3716ec91144513a6b422abd3bca046ef7
nfukada <nfukada@student.42tokyo.jp>
2021-03-05 19:53:05 +0900

    fix(utils): シグナルハンドラで使用する定数を修正

--- srcs/utils/signal.c
+++ srcs/utils/signal.c
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 00:20:59 by nfukada           #+#    #+#             */
-/*   Updated: 2021/03/05 16:46:46 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/03/05 18:13:45 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void		handle_signal(int signal)
 	int			prev_errno;
 
 	prev_errno = errno;
-	ft_putstr_fd(ERASE_SIGNAL_MSG, STDERR_FILENO);
+	ft_putstr_fd(BACK_CURSOR, STDERR_FILENO);
+	ft_putstr_fd(CLEAR_FROM_CURSOR, STDERR_FILENO);
 	if (signal == SIGINT)
 	{
 		ft_putstr_fd("\n"SHELL_PROMPT, STDERR_FILENO);
