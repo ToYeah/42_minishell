27393dc9a826eaddc19181b23e932501c953e8ec
nfukada <nfukada@student.42tokyo.jp>
2021-03-05 20:43:35 +0900

    style(exec): norm対応。wait_commandsを関数分割。command.cに移動

--- srcs/exec/command.c
+++ srcs/exec/command.c
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 17:08:55 by nfukada           #+#    #+#             */
-/*   Updated: 2021/03/02 20:45:36 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/03/05 20:39:43 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,48 @@ t_bool		convert_tokens(t_command *command, char ***args)
 		return (FALSE);
 	}
 	return (TRUE);
+}
+
+static void	handle_command_status(int status, t_bool catch_sigint)
+{
+	extern int	g_status;
+	int			signal;
+
+	if (WIFEXITED(status))
+		g_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+	{
+		signal = WTERMSIG(status);
+		if (signal == SIGQUIT)
+			ft_putendl_fd("Quit: 3", STDERR_FILENO);
+		g_status = signal + 128;
+	}
+	if (catch_sigint)
+		ft_putendl_fd("", STDERR_FILENO);
+}
+
+void		wait_commands(t_command *command)
+{
+	extern int	g_status;
+	int			status;
+	t_bool		has_child;
+	t_bool		catch_sigint;
+
+	has_child = FALSE;
+	catch_sigint = FALSE;
+	while (command)
+	{
+		if (command->pid != NO_PID)
+		{
+			if (waitpid(command->pid, &status, 0) < 0)
+				error_exit(NULL);
+			if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
+				catch_sigint = TRUE;
+			has_child = TRUE;
+		}
+		command = command->next;
+	}
+	if (has_child == FALSE)
+		return ;
+	handle_command_status(status, catch_sigint);
 }
--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 17:59:52 by nfukada           #+#    #+#             */
-/*   Updated: 2021/03/05 16:20:53 by nfukada          ###   ########.fr       */
+/*   Updated: 2021/03/05 20:36:29 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,6 @@
 #include <sys/wait.h>
 #include "parser.h"
 #include "utils.h"
-
-static void		wait_commands(t_command *command)
-{
-	extern int	g_status;
-	int			status;
-	t_bool		has_child;
-	t_bool		catch_sigint;
-	int			signal;
-
-	has_child = FALSE;
-	catch_sigint = FALSE;
-	while (command)
-	{
-		if (command->pid != NO_PID)
-		{
-			if (waitpid(command->pid, &status, 0) < 0)
-				error_exit(NULL);
-			if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-				catch_sigint = TRUE;
-			has_child = TRUE;
-		}
-		command = command->next;
-	}
-	if (has_child == FALSE)
-		return ;
-	if (WIFEXITED(status))
-		g_status = WEXITSTATUS(status);
-	else if (WIFSIGNALED(status))
-	{
-		signal = WTERMSIG(status);
-		if (signal == SIGQUIT)
-			ft_putendl_fd("Quit: 3", STDERR_FILENO);
-		g_status = signal + 128;
-	}
-	if (catch_sigint)
-		ft_putendl_fd("", STDERR_FILENO);
-}
 
 static void		exec_pipeline(t_node *nodes)
 {
