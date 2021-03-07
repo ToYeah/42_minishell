e9d95d1867abbc8e0eecc9b0ac45137da82fb430
nfukada <nfukada@student.42tokyo.jp>
2021-03-07 21:36:39 +0900

    style(builtin): builtin.hの関数順序変更(builtin系を先頭に)

--- includes/builtin.h
+++ includes/builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: nfukada <nfukada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 20:07:55 by nfukada           #+#    #+#             */
-/*   Updated: 2021/03/07 11:11:06 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/03/07 20:36:22 by nfukada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@
 # include "libft.h"
 
 int			is_builtin(char **args);
+int			exec_builtin(char **args);
+
 int			exec_echo(char **args);
 int			exec_exit(char **args);
-int			exec_builtin(char **args);
 int			exec_cd(char **args);
 int			exec_pwd(void);
 void		bind_pwd_value(void);
