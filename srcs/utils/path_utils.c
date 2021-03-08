085ec6f386608a51754dfa7fad8db95f1a4c45c6
Taisei Toya <tytaisei0102@gmail.com>
2021-03-08 08:40:07 +0100

    refactor(path_utils): 使用してない関数を削除

--- srcs/utils/path_utils.c
+++ srcs/utils/path_utils.c
@@ -6,40 +6,12 @@
 /*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 15:32:52 by totaisei          #+#    #+#             */
-/*   Updated: 2021/03/08 08:33:30 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/03/08 08:39:32 by totaisei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 #include "libft.h"
-
-void	correct_path(char **str)
-{
-	char	**split;
-	char	*res;
-	size_t	index;
-
-	if (!str || !*str)
-		return ;
-	index = 0;
-	split = NULL;
-	res = NULL;
-	if (!(split = ft_split(*str, '/')) ||
-		!(res = malloc(sizeof(char) * (ft_strlen(*str) + 1))))
-		error_exit(NULL);
-	ft_safe_free_char(str);
-	*str = res;
-	*res++ = '/';
-	while (split[index])
-	{
-		if (index != 0)
-			*res++ = '/';
-		res = ft_strcpy_forward(res, split[index]);
-		index++;
-	}
-	*res = '\0';
-	ft_safe_free_split(&split);
-}
 
 char	*join_path(const char *prev, const char *next)
 {
